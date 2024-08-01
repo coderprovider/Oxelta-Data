7f7a771b83159a9b1f8ec0dfa16115b4907f4641
coderprovider <white.pury1114@gmail.com>
2024-08-01 15:59:01 +0200

    chore: update page title in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sondge Oxelta",
+  title: "Data Room Oxelta",
   description: "Oxelta, the future of gaming with Web 3. Play and earn OXLT tokens with our innovative ecosystem of play-and-earn games, Web 3, NFTs, blockchain games, crypto rewards, and game economy",
 };
 
